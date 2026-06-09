f0d0a1a0a51e6935fb1c6af3afc5dcf312e1555d
Dmitriy <122107908+NxRDeagle@users.noreply.github.com>
2026-06-10 03:23:51 +0500

    find n unique integers sum up to zero

--- leetcode-cpp/13-count-good-nodes-in-binary-tree.cpp
+++ leetcode-cpp/13-count-good-nodes-in-binary-tree.cpp
@@ -28,6 +28,7 @@ public:
 	}
 };
 
+#ifdef ACTIVE_MAIN
 int main() {
 	Solution sl;
 
@@ -40,6 +41,7 @@ int main() {
 
 	return 0;
 }
+#endif
 
 //Given a binary tree root, a node X in the tree is named good if in the path from root to X there are no nodes with a value greater than X.
 //Return the number of good nodes in the binary tree.
